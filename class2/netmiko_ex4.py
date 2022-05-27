84d35de8cb536fac635413f87ffe8aa71aebfebc
ADMSV_SYS_jglane <Jannik.Glane@gmh-gruppe.de>
2022-05-27 15:53:21 +0200

    started class 2 exercises

---
+++ class2/netmiko_ex4.py
@@ -0,0 +1,27 @@
+import os
+from netmiko import ConnectHandler
+from getpass import getpass
+from datetime import datetime
+
+password = os.getenv("PYNET_PASSWORD") if os.getenv("PYNET_PASSWORD") else getpass()
+
+device = {
+    "host": "cisco4.lasthop.io",
+    "username": "pyclass",
+    "password": password,
+    "device_type": "cisco_nxos",
+    "global_delay_factor": 2
+    #"session_log": "log.txt"
+}
+
+net_connect = ConnectHandler(**device)
+
+commands = [
+    "ip name-server 1.1.1.1",
+    "ip name-server 1.0.0.1",
+    "ip domain-lookup"
+]
+
+output = net_connect.send_config_set(commands)
+
+net_connect.disconnect()
